1e926138290c4d5149b6c78902deb9c14fe6cd3e
JacquiVA02 <a01736339@tec.mx>
2025-03-28 12:39:27 -0600

    Primera prueba String: Correcta

--- clite.py
+++ clite.py
@@ -1,11 +1,16 @@
 import ply.lex as lex
 
-tokens = [ 'FLOAT' , 'INT']
+tokens = [ 'STR' , 'FLOAT' ,'INT' ]
 
 t_ignore  = ' \t'
 
+def t_STR(t):
+  r'[a-zA-Z]+(\s[a-zA-Z]+)+'
+  t.value = str(t.value)
+  return t
+
 def t_FLOAT(t):
-  r'([0-9]+([_][0-9]+)?([.][0-9]*)?([eE][+-]?[0-9]+([_][0-9])?)?|[.][0-9]+([eE][+-][0-9]+)?)'
+  r'([0-9]+([_][0-9]+)*[.][0-9]*([eE][+-]?[0-9]+([_][0-9])?)?|[0-9]+([_][0-9]+)*[eE][+-]?[0-9]+([_][0-9]+)?|[.][0-9]+([eE][+-][0-9]+)?)'
   t.value = float(t.value)
   return t
 
@@ -13,7 +18,8 @@ def t_INT(t):
   r'[0-9]+([_][0-9]+)*'
   t.value = int(t.value)
   return t
-  
+
+
 
 def getLexer():
   return lex.lex()
